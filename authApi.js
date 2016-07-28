665954b17772273c60926508a7e539f7cfa2328e
paulxuca <paulxuca@gmail.com>
2016-07-28 14:40:52 -0400

    Fixed the tale of the missing key in SQL

--- authApi.js
+++ authApi.js
@@ -87,9 +87,6 @@ module.exports = function(app){
               if(err) console.log(err);
               if(!err) response.sendFile(__dirname + '/utils/closeWindow.html');
             });
-          })
-          .catch(function(err){
-            if(err) console.log(err);
           });
         }
       });
