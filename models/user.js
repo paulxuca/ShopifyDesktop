afe4a7d75dbfe882fea0acea182ff61cac27e016
paulxuca <paulxuca@gmail.com>
2016-07-28 09:30:02 -0400

    Models

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ var supergoose = require('supergoose');
 
 var userSchema = new mongoose.Schema({
   storeName: String,
-  accessToken: String
+  accessToken: String,
+  events:[String]
 });
 
 userSchema.plugin(supergoose, { instance: mongoose });
